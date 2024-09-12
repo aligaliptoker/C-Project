4d8cf4b8919c61ff72488c64c5b51b62b190d996
Ali Galip TOKER <atoker@gesislab.com>
2024-09-12 21:13:26 +0300

    first Commit

---
+++ HelloWorld/helloWorld/helloWorld/helloWorld.cpp
@@ -0,0 +1,16 @@
+
+// Author : AGT
+// Created On : 12/09/2024
+
+
+#include <iostream>
+
+int cout = 5;
+
+int main() 
+{
+	std::cout << "Hello World" << std::endl;
+	std::cout << "cout degiskeni:" << cout << std::endl;
+	return 0;
+}
+
