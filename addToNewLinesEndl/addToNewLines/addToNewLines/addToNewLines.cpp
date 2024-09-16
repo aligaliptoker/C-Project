d8c25a02dce46262536f4a3a5bf72bef9e3058fe
Ali Galip TOKER <atoker@gesislab.com>
2024-09-16 22:00:48 +0300

    using the endl for new line

---
+++ addToNewLinesEndl/addToNewLines/addToNewLines/addToNewLines.cpp
@@ -0,0 +1,16 @@
+
+// Author : AGT
+// Created on : 16/09/2024
+// "endl" is used in this file
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello world" << endl; // you can move to the next line again with "endl"
+	cout << "Merhaba Dunya";
+}
+
