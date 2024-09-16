eb48189ac20df94385e8c602a1b43692a7cf45e6
Ali Galip TOKER <atoker@gesislab.com>
2024-09-16 21:20:36 +0300

    multi espression file

---
+++ multiExpression/multiExpression/multiExpression/multiExpression.cpp
@@ -0,0 +1,16 @@
+
+// Author : AGT
+// Created on : 16/09/2024
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello world\n";
+	cout << "Merhaba Dunya";
+	cout << " Bonjour le monde";
+
+}
+
