2ddc1484e126e4c2e7691d3af066043f7a60e241
Ali Galip TOKER <atoker@gesislab.com>
2024-09-23 23:45:52 +0300

    real life examples

---
+++ constants/constants_Variable/constants_Variable/constants_Variable.cpp
@@ -0,0 +1,19 @@
+
+// Author : AGT
+// Created on : 18/09/2024
+
+// const variables are used in this file == bu dosyada deðiþtirilemez deðerler kullanýlmýþtýr
+
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+	const int myConstVariable;
+	
+
+
+}
---
+++ real_Life_Examples/real_Life_Examples/real_Life_Examples/real_Life_Examples.cpp
@@ -0,0 +1,27 @@
+
+// Author : AGT
+// Created On : 23/09/2024
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	// student data
+	int studentId = 15;
+	int studentAge = 23;
+	float studentFee = 75.25;
+	char studentGrade = 'B';
+
+
+	// print variables
+
+	cout << "Student ID:" << studentId << "\n";
+	cout << "Student Age:" << studentAge << "\n";
+	cout << "Student fee:" << studentFee << "\n";
+	cout << "Student Grade:" << studentGrade << "\n";
+}
+
