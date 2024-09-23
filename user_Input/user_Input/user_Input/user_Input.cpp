0c6b03d3cc7cd52f21e648979e8964a8aaea039b
Ali Galip TOKER <atoker@gesislab.com>
2024-09-23 23:57:03 +0300

    user input example

---
+++ user_Input/user_Input/user_Input/user_Input.cpp
@@ -0,0 +1,23 @@
+
+// Author : AGT
+// Created On : 23/09/2024
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	cout << "Type a number:";
+	cin >> x;
+	cout << "your number is:" <<x;
+
+}
+
+
+
+
+
+
+
+
