01c23151a1e6b13a024a5542c002e31ea315ebb7
Ali Galip TOKER <atoker@gesislab.com>
2024-09-24 00:03:21 +0300

    using_input_sum_Example

---
+++ using_input_sum_Example/using_input_sum_Example/using_input_sum_Example/using_input_sum_Example.cpp
@@ -0,0 +1,26 @@
+
+// Author : AGT
+// Created On : 24/09/2024
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, y;
+
+	cout << "Type a number:";
+	cin >> x;
+
+	cout << "Type b number:";
+	cin >> y;
+
+	int sum;
+
+	sum = x + y;
+
+	cout << "sum:" << sum;
+
+}
+
